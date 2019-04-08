829fa12013e031c258cf09a747eb4d8a0ac655ed
Jeffrey Blayney <jblayney@xpanxion.com>
2019-04-08 10:34:10 -0600

    Now its broken after following the toastr injection tutorial for a second time

--- src/app/user/profile.component.ts
+++ src/app/user/profile.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { routerNgProbeToken } from '@angular/router/src/router_module';
+import { Toastr, TOASTR_TOKEN } from '../common/toastr.service';
 
 
 @Component({
@@ -21,7 +22,7 @@ export class ProfileComponent implements OnInit {
   private firstName: FormControl;
   private lastName: FormControl;
 
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(private authService: AuthService, private router: Router, @Inject(TOASTR_TOKEN) private toastr: Toastr) {
 
   }
 
@@ -41,7 +42,7 @@ export class ProfileComponent implements OnInit {
   saveProfile(formValues: any) {
     if (this.profileForm.valid) {
       this.authService.updateCurrentUser(formValues.firstName, formValues.lastName);
-      this.router.navigate(['events']);
+      this.toastr.success('Profile Saved');
     }
   }
 
