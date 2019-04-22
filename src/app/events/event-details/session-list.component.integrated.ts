297d3d1530d338bfe59a59392750702004ac8134
Jeffrey Blayney <jblayney@xpanxion.com>
2019-04-22 08:20:11 -0600

    Finished the course

---
+++ src/app/events/event-details/session-list.component.integrated.ts
@@ -0,0 +1,65 @@
+import { TestBed, async, ComponentFixture } from '@angular/core/testing';
+import { DebugElement, Component, NO_ERRORS_SCHEMA } from '@angular/core';
+import { SessionListComponent } from './session-list.component';
+import { AuthService } from '../../user/auth.service';
+import { VoterService } from './voter.service';
+import { ISession } from '../shared/event.model';
+// import { By } from '@angular/platform-browser';
+import { UpvoteComponent } from './upvote.component';
+import { DurationPipe } from '../shared';
+import { CollapsibleWellComponent } from 'src/app/common';
+
+describe('SessionListComponent', () => {
+  let fixture: ComponentFixture<SessionListComponent>,
+  component: SessionListComponent,
+  element: HTMLElement,
+  debugEl: DebugElement;
+
+  beforeEach(async(() => {
+    let mockAuthService = { isAuthenticated: () => true };
+    let mockVoterService = {};
+
+    TestBed.configureTestingModule({
+      imports: [],
+      declarations: [
+        SessionListComponent,
+        DurationPipe
+      ],
+      providers: [
+        { provide: AuthService, useValue: mockAuthService },
+        { provide: VoterService, useValue: mockVoterService }
+      ],
+      schemas: [
+        NO_ERRORS_SCHEMA
+      ]
+    });
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SessionListComponent);
+    component = fixture.componentInstance;
+    debugEl = fixture.debugElement;
+    element = fixture.nativeElement; // the HTML Element!!! we can query its children in the DOM & stuff.
+  });
+
+  describe('initial display', () => {
+    it('it should have the correct session title.', () => {
+      component.sessions = <ISession[]>[
+        { id: 3, name: 'Session 1', presenter: 'Joe', duration: 1, level: 'beginner',
+          abstract: 'abstract', voters: ['john', 'bob'] }
+      ];
+      component.filterBy = 'all';
+      component.sortBy = 'name';
+      component.eventId = 4;
+
+      component.ngOnChanges();
+      fixture.detectChanges();
+
+      expect(element.querySelector('[well-title]').textContent)
+      .toContain('Session 1');
+
+      // expect(element.querySelector(By.css('[well-title]')).nativeElement.textContent)
+      // .toContain('Session 1');
+    });
+  });
+});
--- src/app/events/event-details/voter.service.ts
+++ src/app/events/event-details/voter.service.ts
@@ -12,7 +12,7 @@ export class VoterService {
   deleteVoter(eventId: number, session: ISession, voterName: string) {
     session.voters = session.voters.filter(voter => voter !== voterName);
 
-    const url = '/api/events/${eventId}/sessions/${session.id}/voters/${voterName}';
+    const url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
     this.http.delete(url).pipe(catchError(this.handleError('deleteVoter'))).subscribe();
   }
 
@@ -20,11 +20,11 @@ export class VoterService {
     session.voters.push(voterName);
 
     const options = { headers: new HttpHeaders({'Content-Type': 'application/json'})};
-    const url = '/api/events/${eventId}/sessions/${session.id}/voters/${voterName}';
+    const url = `/api/events/${eventId}/sessions/${session.id}/voters/${voterName}`;
     this.http.post(url, {}, options).pipe(catchError(this.handleError('addVoter'))).subscribe();
   }
 
-  userHasVoted(session:ISession, voterName: string) {
+  userHasVoted(session: ISession, voterName: string) {
     return session.voters.some(voter => voter === voterName);
   }
 
